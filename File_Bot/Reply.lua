c64e8b5285dbc5b6604c6d7fc5b187d1a447da62
teem44 <82727101+teem44@users.noreply.github.com>
2021-04-19 10:10:55 +0200

    Update Reply.lua

--- File_Bot/Reply.lua
+++ File_Bot/Reply.lua
@@ -2,6 +2,11 @@ local function Reply(msg)
 local text = msg.content_.text_
 if not database:get(bot_id..'Reply:Status'..msg.chat_id_) then
 
+if text == 'نننا' then
+send(msg.chat_id_,msg.id_, 'انت مجرد عضو ملوش لازمه 😹 ') 
+return false
+end
+
 if text == 'تفعيل ردود البوت' and Manager(msg) then
 database:del(bot_id..'Reply:Status'..msg.chat_id_)
 send(msg.chat_id_, msg.id_,'℘︙تم تفعيل ردود البوت')
